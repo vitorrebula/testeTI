5c89feaf2ce08b92d4f798c9d243cfadec1b581b
Vitor Rebula <devrebula@gmail.com>
2023-11-28 19:36:13 -0300

    iniciando ti 2

---
+++ src/main/java/teste/vercel/app/todosimple/TodosimpleApplication.java
@@ -0,0 +1,13 @@
+package teste.vercel.app.todosimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodosimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodosimpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/teste/vercel/app/todosimple/TodosimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package teste.vercel.app.todosimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
